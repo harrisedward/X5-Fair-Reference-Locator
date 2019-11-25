f21754b6f3bc2ca9ec127dfbe46cd9efb5092ef4
harrisedward <harrisocedward@outlook.com>
2019-11-25 21:48:22 +0000

    adding-cards

--- screens/DocumentScreen.js
+++ screens/DocumentScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, StyleSheet, StatusBar } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import axios from 'axios';
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, Text, Body } from 'native-base';
 
 
 export default class LinksScreen extends React.Component {
@@ -48,29 +50,45 @@ export default class LinksScreen extends React.Component {
   		})
   }
 
-  render() {
-  	
-  	const { response, hasLoaded  } = this.state
-  	var test = hasLoaded ? response.oer_materials[0] : null;
-  
-  	return (
-    	 <React.Fragment>
-    		<StatusBar backgroundColor={'red'} barStyle={'dark-content'} translucent={false} />
-        {hasLoaded ? (
-          <React.Fragment>
-            <Text style={styles.baseText}>creation date:</Text>
-            <Text style={styles.baseText}>{test.creation_date}</Text>
-            <Text style={styles.baseText}>description</Text>
-            <Text style={styles.baseText}>{test.description}</Text>
-          </React.Fragment>
-        ) : (
-          <Text style={styles.baseText}>loading...</Text>
-        )}
-      </React.Fragment>
-  	);
-  }
+async componentWillMount() {
+  await Expo.Font.loadAsync({
+    'Roboto': require('native-base/Fonts/Roboto.ttf'),
+    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+  });
 }
 
+export deafult class CardHeaderExample extends Component {
+	const { response, hasLoaded  } = this.state
+  	var test = hasLoaded ? response.oer_materials[0] : null;
+  
+  render() {
+  
+  const { response, hasLoaded  } = this.state
+  	var test = hasLoaded ? response.oer_materials[0] : null;
+  	
+    return (
+		<Header />
+        	<Content>
+          		<Card>
+            		<CardItem header>
+              			<Text>{test.header}</Text>
+            		</CardItem>
+            	<CardItem>
+              		<Body>
+                		<Text>
+                  	<Text numberOfLines={1} style={{ width: 100 }}>{test.description}<Text>		
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem footer>
+              <Text>{test.creation_date}</Text>
+            </CardItem>
+         </Card>
+        </Content>
+    </Container>
+    );
+   } 
+}
 LinksScreen.navigationOptions = {
   title: 'Links',
 };
