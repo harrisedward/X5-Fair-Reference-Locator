3bce1c5f267133ea952391e605c85354956b4e68
harrisedward <harrisocedward@outlook.com>
2019-11-26 12:29:50 +0000

    new-screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { Platform } from 'react-native';
 
 import MainTabNavigator from './MainTabNavigator';
+
+import ArticlesScreen from '../screens/ArticlesScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const ArticlesStack = createStackNavigator(
+  {
+    Articles: ArticlesScreen,
+  },
+  config
+);
+
+ArticlesStack.path = '';
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
+    Articles: ArticlesStack
   })
 );
